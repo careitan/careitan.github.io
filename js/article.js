5e8d483cfe1e28ad036dffd49af3b824edad5a30
Allan R <careitan@users.noreply.github.com>
2019-03-20 11:40:38 -0700

    Updated profile page
    Debugged some functionality.

--- js/article.js
+++ js/article.js
@@ -1,9 +1,35 @@
 function LoadArticle(FilePath){
 	
-	var NewHTML = '<object type="text/html" data="' + FilePath + '" ></object>';
-	document.getElementById("article_view").innerHTML=NewHTML;
+	var NewEl = document.createElement('object');
+	var position = document.getElementById("article_view");
 
-	/*
-	$('#article_view').load(FilePath);
-	*/
+	/* clear child nodes(s) */
+	while (position.firstChild) 
+	{
+    	position.removeChild(position.firstChild);
+    }
+
+	NewEl.type = 'text/html';
+	NewEl.data = FilePath;
+	NewEl.class = 'post_content';
+	NewEl.id = 'articlePanel';
+
+	position.appendChild(NewEl);
+	if (window.masterFilePath != FilePath) {
+		window.masterFilePath = FilePath;
+	}
 };
+
+window.onload = function(){
+	if (window.masterFilePath) {
+		LoadArticle(window.masterFilePath);
+	}
+}
+
+window.onunload = function(){
+	var position = document.getElementById("articlePanel");
+
+	if (window.masterFilePath != position.data) {
+		window.masterFilePath = position.data;
+	}
+}
